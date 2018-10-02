b0c18c14c9f270379f6183cec31483dd1af6d9f7
Rodrigo Rivera <rivera.rodrigo.dev@gmail.com>
2018-10-02 15:56:41 -0400

    [R-2] Tests for the endpoint searchById

--- src/test/java/UserTests/UserServicesTest.java
+++ src/test/java/UserTests/UserServicesTest.java
@@ -2,6 +2,7 @@ package UserTests;
 
 import com.recipes.DTO.User;
 
+import com.recipes.Exceptions.ResourceNotFoundException;
 import com.recipes.Services.UserServices;
 import org.junit.Assert;
 import org.junit.Test;
@@ -19,4 +20,24 @@ public class UserServicesTest {
         Assert.assertTrue(savedUsers == 1);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void findUserbyIdThrowsIllegalArgumentException()  {
+        UserServices userServices = new UserServices();
+        userServices.findUserbyId(-1);
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void findUserbyIdThrowsResourceNotFoundException()  {
+        UserServices userServices = new UserServices();
+        userServices.findUserbyId(0);
+    }
+
+    @Test
+    public void findUserById() {
+        UserServices userServices = new UserServices();
+        User newUser = new User(1, "fullName", "email", "password");
+        userServices.save(newUser);
+        User foundedUser = userServices.findUserbyId(1);
+        Assert.assertTrue(foundedUser.equals(newUser));
+    }
 }
