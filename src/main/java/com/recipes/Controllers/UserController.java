b739c84aa4e1fa774e33497fbf653de1c1df049e
Rodrigo Rivera <rivera.rodrigo.dev@gmail.com>
2018-10-02 15:54:00 -0400

    [R-2] endpoint added to get user by id

--- src/main/java/com/recipes/Controllers/UserController.java
+++ src/main/java/com/recipes/Controllers/UserController.java
@@ -2,16 +2,15 @@ package com.recipes.Controllers;
 
 import java.util.List;
 import com.recipes.DTO.User;
+import com.recipes.Exceptions.ResourceNotFoundException;
 import com.recipes.Services.IUserServices;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -28,6 +27,19 @@ public class UserController {
         userServices.save(newUser);
         return new HttpEntity<String>(newUser.toString());
     }
+
+    @RequestMapping(value = "/{id}", produces = "application/json", method = RequestMethod.GET)
+    public HttpEntity<User> getUserById(@PathVariable long id) {
+        try {
+            User foundedUser = userServices.findUserbyId(id);
+            return new ResponseEntity<>(foundedUser, HttpStatus.OK);
+        } catch (IllegalArgumentException ilegalArugmentException) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (ResourceNotFoundException resourceNotFoundException) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 
     @RequestMapping(method = RequestMethod.GET)
     public List<User> userList() {
