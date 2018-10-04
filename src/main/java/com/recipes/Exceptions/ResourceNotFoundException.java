e6dc3ab7298fc5f8c654fd05199262dc3a4ec106
Rodrigo Rivera <rivera.rodrigo.dev@gmail.com>
2018-10-04 10:19:34 -0400

    Modify the structure, configurations and remove the backend from the current project

--- src/main/java/com/recipes/Exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.recipes.Exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(Class theClass, long id) {
-        super("The " + theClass.getSimpleName() + " with id " + id + " was not found");
-    }
-}
--- src/main/java/com/recipes/Exceptions/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.recipes.Exceptions;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException () {
-        super("You don't have the permission to execute this action");
-    }
-}
