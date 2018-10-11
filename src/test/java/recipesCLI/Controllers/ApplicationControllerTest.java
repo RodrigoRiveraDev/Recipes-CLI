2fa2fbfaf5061435caa28b82973399192f115a7f
Rodrigo Rivera <rivera.rodrigo.dev@gmail.com>
2018-10-11 09:07:34 -0400

    [CLI-13] base template for tests added

---
+++ src/test/java/recipesCLI/Controllers/ApplicationControllerTest.java
@@ -0,0 +1,20 @@
+package recipesCLI.Controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplicationControllerTest {
+
+    @Test
+    public void enterUserId() {
+    }
+
+    @Test
+    public void getInitInstructions() {
+    }
+
+    @Test
+    public void selectOption() {
+    }
+}
---
+++ src/test/java/recipesCLI/Controllers/UserCLIControllerTest.java
@@ -0,0 +1,28 @@
+package recipesCLI.Controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserCLIControllerTest {
+
+    @Test
+    public void enterUserId() {
+    }
+
+    @Test
+    public void getInitInstructions() {
+    }
+
+    @Test
+    public void register() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void viewAllUser() {
+    }
+}
